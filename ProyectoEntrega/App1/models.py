7c1215b0c2f87deaaee627ad74088173dceefc27
LucasOlhasso <lacompuyvos@gmail.com>
2023-05-03 09:24:56 -0300

    primera

---
+++ ProyectoEntrega/App1/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Estos son los modelos.
+class Pacientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    dni= models.IntegerField()
+
+class Profesional(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    email= models.EmailField()
+    profesion= models.CharField(max_length=30)
+
+class Turno(models.Model):
+    nombrepaciente= models.CharField(max_length=50) #por ahora no están vinculados entre tablas
+    nombreprofesional= models.CharField(max_length=50) #por ahora no están vinculados entre tablas
+    fechaturno= models.DateField()
+    turnovigente= models.BooleanField()
+    
